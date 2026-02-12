10caf4c638c40f41c127da0ffd0354fc1deacc45
Flora <flora.ortiz@ucb.edu.bo>
2026-02-12 08:47:22 -0400

    feat: add to presenter

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,6 @@
-import sumar from "./sumador";
+import { sumar, multiplicar } from "./sumador.js";
 
+// ---------- SUMA ----------
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
@@ -7,9 +8,22 @@ const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+// ---------- MULTIPLICACIÓN ----------
+const firstMul = document.querySelector("#primer-numero-multiplicar");
+const secondMul = document.querySelector("#segundo-numero-multiplicar");
+const formMul = document.querySelector("#multiplicar-form");
+const divMul = document.querySelector("#resultado-multiplicar-div");
+
+formMul.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumberMul = Number.parseInt(firstMul.value);
+  const secondNumberMul = Number.parseInt(secondMul.value);
+
+  divMul.innerHTML = "<p>" + multiplicar(firstNumberMul, secondNumberMul) + "</p>";
 });
